c30a507f90e6f7e593f65191d8072c9ad114995e
EduardoRocha01 <120874588+EduardoRocha01@users.noreply.github.com>
2023-08-19 17:40:27 -0400

    formação do objeto com dadaos para edição

--- JS/produtos.js
+++ JS/produtos.js
@@ -1,100 +1,131 @@
-export class Produtos{
-
-    
-
-    constructor(){
-         
-        
+export class Produtos {
+    constructor() {
+        // Construtor da classe Produtos
     }
 
-
-    cadastrarprod(nome,preco,quantidade,descricao){ // prepara os dados e envia ao PHP
-
+    // Função para cadastrar um produto no servidor
+    cadastrarprod(nome, preco, quantidade, descricao) {
         const dados = {
             nome: nome,
             preco: preco,
             quantidade: quantidade,
-            descricao: descricao
-        }
-
-
+            descricao: descricao,
+        };
 
         $.ajax({
-            url: '../PHP/insertpedidos.php', // Substitua pelo nome do arquivo PHP que irá receber os dados
-            type: 'POST', // Ou 'GET' se preferir uma solicitação GET
-            data: dados, // Os dados a serem enviados para o servidor
+            url: '../PHP/insertpedidos.php',
+            type: 'POST',
+            data: dados,
             dataType: 'json',
-            success: function(data) {
-                // Manipule a resposta JSON recebida do servidor aqui.
+            success: function (data) {
                 console.log('Resposta do servidor:', data);
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.log('Erro na requisição AJAX:', error);
-            }
+            },
         });
+    }
+
+
+
+
+    // Função para exibir os produtos na página
+    exibirprodutos(iddiv, nomeclassdivprod) {
+
         
-
-    } // fim da função cadastrarprod()
-
-
-    exibirprodutos(iddiv,nomeclassdivprod){
+        // Função interna para renderizar todos os produtos
+        function renderizaprod(data) {
+            const divprincipal = document.getElementById(iddiv);
 
 
-        function renderizaprod(data){
+            //  dados necessarios para editar um produto, tudo que estiver aqui serve para editar id = formeditprod
+            const nome = document.getElementById("nomeEdit")
+            const preco = document.getElementById("precoEdit")
+            const quantidadeEdit= document.getElementById("quantidadeEdit")
+            const descricaoEdit = document.getElementById("descricaoEdit")
+            //  dados necessarios para editar um produto, tudo que estiver aqui serve para editar id = formeditprod
 
-            const divprincipal = document.getElementById(iddiv) // div que recebera todos os childs 
 
             data.forEach(item => {
+                const divprod = document.createElement('div');
+                divprod.setAttribute('class', nomeclassdivprod);
+                divprod.setAttribute("data", item.id)
 
-            const divprod = document.createElement("div") // div que recebe preço, nome, descrição, id
-            divprod.setAttribute("class", nomeclassdivprod )
+                // Criação do elemento para a descrição
+                const descricao = document.createElement('p');
+                descricao.textContent = item.descricao;
 
-           
+                const id = document.createElement('p');
+                id.setAttribute("id", "id")
+                id.textContent = item.id;
 
-            const descricao = document.createElement("p")
-            descricao.textContent = `${item.descricao}`
+                // Criação do elemento para o nome do produto
+                const nomeprod = document.createElement('h1');
+                nomeprod.textContent = item.nome;
 
-            const nomeprod = document.createElement("h1")
-            nomeprod.textContent = `${item.nome}`
+                // Criação do elemento para a imagem da quantidade
+                const quantidade = document.createElement('img');
+                quantidade.setAttribute('src', 'https://multilit.com.br/wp-content/uploads/2020/03/Produto-sem-foto.png');
 
-            const quantidade = document.createElement("img")
-            quantidade.setAttribute("src", "https://multilit.com.br/wp-content/uploads/2020/03/Produto-sem-foto.png")
+                // Criação do elemento para o preço
+                const h2preco = document.createElement('h2');
+                h2preco.textContent = 'Preço R$: ' + item.preco;
 
-            const h2preco = document.createElement("h2")
-            h2preco.textContent = `Preco R$: ${item.preco}`
+                const btn_editar = document.createElement('button');
+                btn_editar.textContent = 'Editar';
+                btn_editar.addEventListener('click', () => {
+                    // Aqui você pode implementar a lógica para capturar as informações do usuário para edição
+                    
 
+
+                    const dadosedit = {
+                        id: item.id,
+                        nome : nome.value,
+                        preco : preco.value,
+                        descricao : quantidadeEdit.value,
+                        quantidade : descricaoEdit.value
+                    }
+
+                    console.log(dadosedit)
             
-            divprod.appendChild(nomeprod)
-            divprod.appendChild(descricao)
-            divprod.appendChild(h2preco)
-            divprod.appendChild(quantidade)
-
+                    // Agora você tem as informações que o usuário deseja editar (novadescricao e novopreco)
+                    // e também o ID do produto (item.id)
+                    // Faça o que for necessário com esses dados, como atualizar os valores no servidor, etc.
+                });
             
-            divprincipal.appendChild(divprod)
+                // Botão de Excluir
+                const btn_excluir = document.createElement('button');
+                btn_excluir.textContent = 'Excluir';
+                btn_excluir.addEventListener('click', () => {
+                    // Aqui você pode implementar a lógica para excluir o produto com o ID correspondente (item.id)
+                });
 
-                
+                // Adicionando os elementos criados à div do produto
+                divprod.appendChild(nomeprod);
+                divprod.appendChild(id);
+                divprod.appendChild(btn_editar);
+                divprod.appendChild(btn_excluir);
+                divprod.appendChild(descricao);
+                divprod.appendChild(h2preco);
+                divprod.appendChild(quantidade);
+
+                // Adicionando a div do produto à div principal
+                divprincipal.appendChild(divprod);
             });
+        }
 
-           }/* fim função renderizaprod()*/
-
+        // Fazendo uma requisição AJAX para buscar os produtos no servidor
         $.ajax({
-            url: '../PHP/selectprodutos.php', // Substitua pelo nome do arquivo PHP que irá receber os dados
-            type: 'GET', // Ou 'GET' se preferir uma solicitação GET
+            url: '../PHP/selectprodutos.php',
+            type: 'GET',
             dataType: 'json',
-            success: function(data) {
-                // Manipule a resposta JSON recebida do servidor aqui.
-                console.log(data)
-                renderizaprod(data)
+            success: function (data) {
+                console.log(data);
+                renderizaprod(data);
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.log('Erro na requisição AJAX:', error);
-            }
+            },
         });
-
-
-        
-    } 
-
-
-
-}/*fim da função exibirprodutos()*/
+    }
+}
